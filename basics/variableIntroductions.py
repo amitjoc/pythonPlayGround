5295ef31ec35e9b230df2dc518e3a765370a0d1a
Amit Joshi <amitjoc@gmail.com>
2024-11-09 13:44:40 +0530

    variable introduciton

---
+++ basics/variableIntroductions.py
@@ -0,0 +1,21 @@
+print("-----------------------------------------------")
+print("My name is Amit Joshi")
+print("My age is 26 years")
+
+print("-----------------------------------------------")
+print("I am working as Software Developer")
+print("I am new to Python Language")
+
+# variable declaration 
+person_name = "Amit Joshi"
+person_age = "26"
+
+print("-----------------------------------------------")
+print("My name is " + person_name)
+print("My age is " + person_age +" years")
+
+person_job = "Software Engineer"
+person_language = "Python"
+print("-----------------------------------------------")
+print("I am working as Software Developer")
+print("I am new to "+ person_language + " Language")
