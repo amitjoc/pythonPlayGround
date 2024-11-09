bad8a33dbe5a43dfef29b60f60ad2a1b30db834b
Amit Joshi <amitjoc@gmail.com>
2024-11-10 00:07:29 +0530

    numbers updated

---
+++ basics/numbersIntro.py
@@ -0,0 +1,7 @@
+# Basic number handling in the python 
+
+print(25)               # integer number printing 
+print(-25)              # negative number printing 
+
+print(25.2356)          # flot or double numebr printing
+print(-25.253451)       # negative float or double number printing 
