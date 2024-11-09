53effa51e54089544cd2288db7bade53b873257f
Amit Joshi <amitjoc@gmail.com>
2024-11-09 22:48:34 +0530

    some string functions

---
+++ basics/stringIntro.py
@@ -0,0 +1,8 @@
+example_string = "Jay Shree Ram"
+
+print(example_string.lower())
+print(example_string.upper()) 
+
+print(example_string.upper().isupper())
+print(example_string.lower().islower())
+
